5f9ab1d134afb485b957e2625ea16fc6a1268c48
lokesh-coder <mexican.dirtyfellow@gmail.com>
2020-06-17 09:23:03 +0530

    fix(validator): required rule not working when set to false

--- packages/helpers/validator/src/validators.ts
+++ packages/helpers/validator/src/validators.ts
@@ -1,4 +1,4 @@
-export const requiredValidator = value => !!value;
+export const requiredValidator = (value, rule) => rule === !!value;
 export const exactValidator = (value, rule) => value === rule;
 export const sizeValidator = (value, rule) => {
   return !!value && value.length >= rule.min && value.length <= rule.max;
