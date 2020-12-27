790231308e01cabb0d800961ddb4c4e2575c5c17
lokesh-coder <mexican.dirtyfellow@gmail.com>
2020-12-27 06:59:21 +0530

    fix(lesy-plugin-artist): attach clear timers func and enable plugins support

--- packages/plugins/lesy-plugin-artist/src/artist.feature.ts
+++ packages/plugins/lesy-plugin-artist/src/artist.feature.ts
@@ -1,7 +1,18 @@
-export default function (feature: any) {
-  feature.artist = () => {
+export default function (feature: any, root: any, utils: any) {
+  feature.artist = (config) => {
+    feature.artistInstanceId = Date.now();
     const artistClass = require("@lesy/artist").default;
-    const artist = new artistClass();
+    const artist = new artistClass(utils.color());
+    feature.artistDispose = artist.clearTimers.bind(artist);
+    const pluginConfig = config["@lesy/lesy-plugin-artist"];
+    if (pluginConfig?.plugins) {
+      let elements = {};
+      pluginConfig.plugins.forEach((plugin) => {
+        elements = { ...elements, ...plugin.module.elements };
+      });
+      artist.registerElements(elements);
+    }
+
     return { ui: artist };
   };
 }
